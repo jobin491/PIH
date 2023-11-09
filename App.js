12fedcf7b98f49ca2ae2d49d70335bf32bbcda11
jobin491 <124445728+jobin491@users.noreply.github.com>
2023-11-09 23:43:14 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from "react"
-import Navbar from "./components/navbar"
-import Quote from "./components/quote";
-import Questions from "./components/questions";
-import Extra from "./components/extra";
+import Navbar from "./navbar"
+import Quote from "./quote";
+import Questions from "./questions";
+import Extra from "./extra";
 function App() {
   return (
   <>
